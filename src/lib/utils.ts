1276b36b7ab7e22090f25c9cbf20b5f3683fc219
Junyoung Choi <fluke8259@gmail.com>
2026-03-07 17:19:37 +0900

    Add sleep to debug

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function sleep(duration: number) {
+  return new Promise((res, rej) => {
+    setTimeout(res, duration)
+  })
+}
