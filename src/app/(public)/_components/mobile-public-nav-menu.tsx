2b6439bdb84d08b15d1a2dc5300521f5b85125b1
Junyoung Choi <fluke8259@gmail.com>
2026-06-17 09:17:57 +0900

    Upgrade lucide react

--- src/app/(public)/_components/mobile-public-nav-menu.tsx
+++ src/app/(public)/_components/mobile-public-nav-menu.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Github, Menu } from 'lucide-react'
+import { Menu } from 'lucide-react'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import {
